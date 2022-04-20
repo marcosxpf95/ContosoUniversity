fa7b88ed5d7fa3c8012057ef8185fec02495a868
Marcos Xavier <97961003+dextracreditasmxavier@users.noreply.github.com>
2022-04-20 12:54:54 -0300

    initial copmmit

---
+++ ContosoUniversity1.Lib/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace ContosoUniversity1.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
